386b65901a0fb78fe08a76462f0516e364cbfaf3
kakashibhaiya <chetanflash15@gmail.com>
2024-10-28 01:48:13 +0530

    Earth & Stars are done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,16 +22,78 @@ const Contact = () => {
   const handleSubmit = (e) => {}
 
   return (
-    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
-     <motion.div
-       variants={slideIn('left', "tween", 0.2, 1)}
-       className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-     >
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+    <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
        <p className={styles.sectionSubText}>Get in touch</p>
        <h3 className={styles.sectionHeadText}>Contact</h3>
+
+       <form
+        ref = {formRef}
+        onSubmit={handleSubmit}
+        className="mt-12 flex flex-col gap-8"
+       >
+       
+        <label className="flex flex-col">
+          <span className="text-white font-medium mb-4">Your Name</span>
+          <input 
+            type="text"
+            name="name"
+            //value={form.name}
+            onChange={handleChange}
+            placeholder="What's your name?"
+            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+          />
+        </label>
+
+        <label className="flex flex-col">
+        <span className="text-white font-medium mb-4">Your Email</span>
+        <input 
+          type="email"
+          name="email"
+          //value={form.email}
+          onChange={handleChange}
+          placeholder="What's your email?"
+          className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+        />
+      </label>
+
+      <label className="flex flex-col">
+      <span className="text-white font-medium mb-4">Your Meassage</span>
+      <textarea 
+        rows="7"
+        type="text"
+        name="name"
+        //value={form.message}
+        onChange={handleChange}
+        placeholder="What do you want to say?"
+        className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+      />
+    </label>
+    
+    <button 
+     type="submit"
+     className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+    >
+    {loading ? 'sending...' : 'send'}
+    </button>
+
+       </form>
      
-     </motion.div>
-    </div>
+</motion.div>
+
+<motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
+     
+</div>
   )
 }
 
